5cdcc63a21b448c6856ed41b53a20691e6fd0535
Nathan Garzya Santoso <85006633+canonflow@users.noreply.github.com>
2024-11-26 14:41:05 +0700

    feat(module): Add module 2

--- 02 - Variables, Strings, and Numbers/main.go
+++ 02 - Variables, Strings, and Numbers/main.go
@@ -3,7 +3,39 @@ package main
 import "fmt"
 
 func main() {
-	// Strings
+	// ===== Strings =====
+	fmt.Println("===== STRINGS =====")
 	var nameOne string = "Nathan"
-	fmt.Println(nameOne)
+	var nameTwo = "Garzya"
+	var nameThree string
+
+	fmt.Println(nameOne, nameTwo, nameThree)
+
+	nameOne = "Test"
+	nameThree = "Three"
+	fmt.Println(nameOne, nameTwo, nameThree)
+
+	// Shorthand (without var) [only in a function / can't use it outside a function]
+	nameFour := "Shorthand"
+	fmt.Println(nameFour)
+
+	// ===== Integer =====
+	fmt.Println("\n===== INTEGER =====")
+	var ageOne int = 10
+	var ageTwo = 20
+	ageThree := 30
+
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+	// Bits and Memory
+	var numOne int8 = 25
+	var unsignedNum uint = 0
+	fmt.Println(numOne, unsignedNum)
+
+	// ===== FLOATS =====
+	fmt.Println("\n===== FLOAT =====")
+	var floatOne float32 = 3.14
+	var floatTwo float64 = 2.13123213123213123 // Has a slightly higher precision than float32
+	floatThree := 103.231312312321312          // Default float64
+	fmt.Println(floatOne, floatTwo, floatThree)
 }
