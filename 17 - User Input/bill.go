d6c6715be31563624a57c56440a1a4044e64d3ee
Nathan Garzya Santoso <85006633+canonflow@users.noreply.github.com>
2024-11-30 23:06:30 +0700

    feat(module): Add module 17

---
+++ 17 - User Input/bill.go
@@ -0,0 +1,60 @@
+package main
+
+import "fmt"
+
+type bill struct {
+	name  string
+	items map[string]float64
+	tip   float64
+}
+
+// Make new bills
+func newBill(name string) bill {
+	b := bill{
+		name:  name,
+		items: map[string]float64{},
+		tip:   0,
+	}
+
+	return b
+}
+
+// Format the bill
+func (b *bill) format() string {
+	fs := "Bill breakdown: \n"
+	var total float64 = 0
+
+	// List items
+	for k, v := range b.items {
+		fs += fmt.Sprintf("%-25v ... $%.2f \n", k+":", v)
+		total += v
+	}
+
+	// Add tip
+	fs += fmt.Sprintf("%-25v ... $%.2f \n", "Tip:", b.tip)
+	total += b.tip
+
+	// Total
+	// -25 add empty space on right
+	fs += fmt.Sprintf("%-25v ... $%.2f", "Total:", total)
+
+	return fs
+	/*
+		Bill breakdown:
+		pie:                      ... $5.99
+		cake:                     ... $3.99
+		Total:                    ... 9.98
+	*/
+}
+
+// Update tip
+func (b *bill) updateTip(tip float64) {
+	//(*b).tip = tip // not mandatory for struct
+	b.tip = tip
+}
+
+// Add an item to the bill
+func (b *bill) addItem(name string, price float64) {
+	//(*b).items[name] = price // not mandatory for struct
+	b.items[name] = price
+}
